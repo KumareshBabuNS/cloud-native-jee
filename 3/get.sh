cf57a448a8a5915f5153dbdfe19274b29bfd5801
Mike <backtotheoldhouse@gmail.com>
2016-05-21 08:33:04 +0100

    moved scripts to within the Vagrant dir

--- 3/get.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash - 
-set -o nounset                              # Treat unset variables as an error
-
-IP=${1-127.0.0.1}
-PORT=${2-8080}
-
-curl http://$IP:$PORT/rest-jcache/webresources/cache?key=test
-
